46b4973adc7a80904669ebb66a48619470915333
Yu-Ji19 <ji.yu.vy@gmail.com>
2019-10-26 01:18:55 -0400

    added build

---
+++ build/precache-manifest.76fc2bf5a617804f7c0fc940069ff6d0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6141e6a1e6b670708f5b76f737cbba9c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e5a0167cef764289eb8b",
+    "url": "/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "d7640cdff5bbf27005f1",
+    "url": "/static/css/main.f05016c4.chunk.css"
+  },
+  {
+    "revision": "e5a0167cef764289eb8b",
+    "url": "/static/js/2.cee0e367.chunk.js"
+  },
+  {
+    "revision": "d7640cdff5bbf27005f1",
+    "url": "/static/js/main.a65c7f5d.chunk.js"
+  },
+  {
+    "revision": "6dcb1ef5710a0ff17314",
+    "url": "/static/js/runtime-main.25541922.js"
+  }
+]);
